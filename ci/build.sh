f2eeed5c64464dd177f436d77fc0d3a84dc7fc60
Greg Turnquist <gturnquist@pivotal.io>
2019-12-10 14:06:08 -0600

    Backport Jenkins migration to 0.8.x.
    [resolves #50]

--- ci/build.sh
+++ ci/build.sh
@@ -1,15 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -euo pipefail
 
-[[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
-
-r2dbc_proxy_artifactory=$(pwd)/r2dbc-proxy-artifactory
-r2dbc_spi_artifactory=$(pwd)/r2dbc-spi-artifactory
-
-rm -rf $HOME/.m2/repository/io/r2dbc 2> /dev/null || :
-
-cd r2dbc-proxy
-./mvnw deploy \
-    -DaltDeploymentRepository=distribution::default::file://${r2dbc_proxy_artifactory} \
-    -Dr2dbcSpiArtifactory=file://${r2dbc_spi_artifactory}
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/r2dbc-proxy-maven-repository" ./mvnw -P${PROFILE} -Dmaven.test.skip=true clean deploy -B
