84239537764e6b4242e692bcad8157199e711d83
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2022-11-10 17:58:51 -0800

    Port initial observability implementation
    Initial implementation retrieved from "data-reactive" in "micrometer-samples".
    https://github.com/micrometer-metrics/micrometer-samples/tree/main/data-reactive

---
+++ src/main/java/io/r2dbc/proxy/observation/ObservationProxyExecutionListener.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.r2dbc.proxy.observation;
+
+import io.micrometer.common.util.internal.logging.InternalLogger;
+import io.micrometer.common.util.internal.logging.InternalLoggerFactory;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.contextpropagation.ObservationThreadLocalAccessor;
+import io.micrometer.observation.transport.Kind;
+import io.micrometer.observation.transport.SenderContext;
+import io.r2dbc.proxy.callback.DelegatingContextView;
+import io.r2dbc.proxy.core.QueryExecutionInfo;
+import io.r2dbc.proxy.core.QueryInfo;
+import io.r2dbc.proxy.listener.ProxyExecutionListener;
+import io.r2dbc.spi.ConnectionFactory;
+import reactor.util.context.Context;
+import reactor.util.context.ContextView;
+
+/**
+ * @author Marcin Grzejszczak
+ * @author Tadaya Tsuyukubo
+ */
+public class ObservationProxyExecutionListener implements ProxyExecutionListener {
+
+    private static final InternalLogger log = InternalLoggerFactory.getInstance(ObservationProxyExecutionListener.class);
+
+    private final ConnectionFactory connectionFactory;
+
+    private final ObservationRegistry observationRegistry;
+
+    private final String url;
+
+    public ObservationProxyExecutionListener(ObservationRegistry observationRegistry,
+                                             ConnectionFactory connectionFactory, String url) {
+        this.observationRegistry = observationRegistry;
+        this.connectionFactory = connectionFactory;
+        this.url = url;
+    }
+
+    @Override
+    public void beforeQuery(QueryExecutionInfo executionInfo) {
+        if (this.observationRegistry.isNoop()) {
+            return;
+        }
+        Observation parentObservation = executionInfo.getValueStore()
+            .getOrDefault(ContextView.class, new DelegatingContextView(Context.empty()))
+            .getOrDefault(ObservationThreadLocalAccessor.KEY, this.observationRegistry.getCurrentObservation());
+        if (parentObservation == null) {
+            if (log.isDebugEnabled()) {
+                log.debug("Parent observation not present, won't do any instrumentation");
+            }
+            return;
+        }
+        String name = this.connectionFactory.getMetadata().getName();
+        Observation observation = clientObservation(parentObservation, executionInfo, name);
+        if (log.isDebugEnabled()) {
+            log.debug("Created a new child observation before query [" + observation + "]");
+        }
+        tagQueries(executionInfo, observation);
+        executionInfo.getValueStore().put(Observation.class, observation);
+    }
+
+    Observation clientObservation(Observation parentObservation, QueryExecutionInfo executionInfo, String name) {
+        // @formatter:off
+        SenderContext<?> context = new SenderContext<>((carrier, key, value) -> { }, Kind.CLIENT);
+        context.setRemoteServiceName(name);
+        context.setRemoteServiceAddress(this.url);
+        Observation observation = R2DbcObservationDocumentation.R2DBC_QUERY_OBSERVATION.observation(this.observationRegistry, () -> context)
+            .parentObservation(parentObservation)
+            .lowCardinalityKeyValue(R2DbcObservationDocumentation.LowCardinalityKeys.CONNECTION.withValue(name))
+            .lowCardinalityKeyValue(R2DbcObservationDocumentation.LowCardinalityKeys.THREAD.withValue(executionInfo.getThreadName()));
+        // @formatter:on
+        return observation.start();
+    }
+
+    private void tagQueries(QueryExecutionInfo executionInfo, Observation observation) {
+        int i = 0;
+        for (QueryInfo queryInfo : executionInfo.getQueries()) {
+            observation.highCardinalityKeyValue(
+                String.format(R2DbcObservationDocumentation.HighCardinalityKeys.QUERY.name(), i),
+                queryInfo.getQuery());
+            // TODO: query params
+            i = i + 1;
+        }
+
+    }
+
+
+
+
+    @Override
+    public void afterQuery(QueryExecutionInfo executionInfo) {
+        Observation observation = executionInfo.getValueStore().get(Observation.class, Observation.class);
+        if (observation != null) {
+            if (log.isDebugEnabled()) {
+                log.debug("Continued the child observation in after query [" + observation + "]");
+            }
+            final Throwable throwable = executionInfo.getThrowable();
+            if (throwable != null) {
+                observation.error(throwable);
+            }
+            observation.stop();
+        }
+    }
+
+    @Override
+    public void eachQueryResult(QueryExecutionInfo executionInfo) {
+        Observation observation = executionInfo.getValueStore().get(Observation.class, Observation.class);
+        if (observation != null) {
+            if (log.isDebugEnabled()) {
+                log.debug("Marking after query result for observation [" + observation + "]");
+            }
+            observation.event(R2DbcObservationDocumentation.Events.QUERY_RESULT);
+        }
+    }
+
+}
---
+++ src/main/java/io/r2dbc/proxy/observation/R2DbcObservationDocumentation.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.r2dbc.proxy.observation;
+
+import io.micrometer.common.docs.KeyName;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.docs.ObservationDocumentation;
+
+/**
+ * @author Marcin Grzejszczak
+ * @author Tadaya Tsuyukubo
+ */
+public enum R2DbcObservationDocumentation implements ObservationDocumentation {
+
+    R2DBC_QUERY_OBSERVATION {
+        @Override
+        public String getName() {
+            return "r2dbc.query";
+        }
+
+        @Override
+        public String getContextualName() {
+            return "query";
+        }
+
+        @Override
+        public KeyName[] getLowCardinalityKeyNames() {
+            return LowCardinalityKeys.values();
+        }
+
+        @Override
+        public KeyName[] getHighCardinalityKeyNames() {
+            return HighCardinalityKeys.values();
+        }
+
+        @Override
+        public Observation.Event[] getEvents() {
+            return Events.values();
+        }
+
+        @Override
+        public String getPrefix() {
+            return "r2dbc.";
+        }
+    };
+
+    enum Events implements Observation.Event {
+
+        /**
+         * Annotated before executing a method annotated with @ContinueSpan or @NewSpan.
+         */
+        QUERY_RESULT {
+            @Override
+            public String getName() {
+                return "r2dbc.query_result";
+            }
+        }
+
+    }
+
+    enum LowCardinalityKeys implements KeyName {
+
+        /**
+         * Name of the R2DBC connection.
+         */
+        CONNECTION {
+            @Override
+            public String asString() {
+                return "r2dbc.connection";
+            }
+        },
+
+        /**
+         * Name of the R2DBC thread.
+         */
+        THREAD {
+            @Override
+            public String asString() {
+                return "r2dbc.thread";
+            }
+        }
+
+    }
+
+    enum HighCardinalityKeys implements KeyName {
+
+        /**
+         * Name of the R2DBC query.
+         */
+        QUERY {
+            @Override
+            public String asString() {
+                return "r2dbc.query[%s]";
+            }
+        },
+
+    }
+
+}
